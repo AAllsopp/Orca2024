5b2e0ad74e8f3a8ca47b64a6112bdcaabb01d82e
gtamb <gillian.tamblyn@icloud.com>
2024-10-19 15:46:56 -0700

    Feat: eat planets

--- Assets/Scripts/character.cs
+++ Assets/Scripts/character.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+[RequireComponent(typeof(Rigidbody))]
 
 public class character : MonoBehaviour
 {   
@@ -10,6 +11,10 @@ public class character : MonoBehaviour
     private bool playerGround; 
     private float jumpHeight = 1.0f;
     private float gravityValue = -9.81f;
+    private Vector3 playerSize = new Vector3 (1,1,1);
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -38,5 +43,43 @@ public class character : MonoBehaviour
         }
         playerVel.y += gravityValue*Time.deltaTime;
         charController.Move(playerVel*Time.deltaTime);
+    }
+
+    
+
+    // ?? is this the correct ControllerColliderHit 
+    void OnControllerColliderHit(ControllerColliderHit hit){
+
+       // Rigidbody food = hit.collider.attachedRigidBody;
+       // if(food == null){
+         //   return;
+       // }
+        // get an attribute for size of collision 
+        //Vector3 size = FindFirstObjectByType<planetFood>().giveSize();
+      
+
+        if(hit.gameObject.CompareTag("Food")){
+            Vector3 foodSize = hit.collider.bounds.size;
+            if(playerSize.x >= foodSize.x){
+                
+                playerSize.x += foodSize.x;
+                playerSize.y += foodSize.y;
+                playerSize.z += foodSize.z;
+                Destroy(hit.gameObject);
+                
+            }
+
+        }
+       
+
+        //float jumpOverlap;
+       // if (playerSize >= 2) {
+            // If player is not small, they can jump on the mushroom
+            //jumpOverlap = Physics.OverlapSphere(jumpCheckPos.position, 0.05f, LayerMask.GetMask("Ground", "ShroomBounce")).Length;
+       // } else {
+         //   // If the player is small, they cannot jump on the mushroom
+         //   jumpOverlap = Physics.OverlapSphere(jumpCheckPos.position, 0.05f, LayerMask.GetMask("Ground")).Length;
+        //}
+        
     }
 }
